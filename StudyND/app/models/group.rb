f6392d960983e8ff99b8ddf1894cdc9b8e7aa8d3
ecervant <ecervant@nd.edu>
2016-12-14 01:20:56 -0500

    validations for group

--- StudyND/app/models/group.rb
+++ StudyND/app/models/group.rb
@@ -2,4 +2,12 @@ class Group < ActiveRecord::Base
   belongs_to :course
   has_many :users
   has_many :ratings
+  
+  validates :name, presence: true
+  validates :name, uniqueness: true
+
+  def to_s
+    name
+  end
+  
 end
