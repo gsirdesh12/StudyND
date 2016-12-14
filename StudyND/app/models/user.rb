74f7d0fa64b8da9c93480688a6624f6ca3cf1390
ecervant <ecervant@nd.edu>
2016-12-14 01:16:18 -0500

    Validations for user

--- StudyND/app/models/user.rb
+++ StudyND/app/models/user.rb
@@ -2,4 +2,7 @@ class User < ActiveRecord::Base
   has_many :enrollments
   has_many :groups, through: :enrollments
   has_many :ratings
+  
+  validates :first_name, :last_name, :email, :password, :grade, presence: true
+  validates :grade, inclusion: { in: 1..4, message:  "\"%{value}\" must be between 1 and 4: 1 is First-Year, 2 is Sophomore, etc." }
 end
