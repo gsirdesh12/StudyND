84294de26646a5a331dec8ae2ff4c5349bc36e4a
Gaurav Sirdeshpande <gsirdesh@nd.edu>
2016-12-14 13:38:52 -0500

    Added custom method and scopes to User model

--- StudyND/app/models/user.rb
+++ StudyND/app/models/user.rb
@@ -2,7 +2,18 @@ class User < ActiveRecord::Base
   has_many :enrollments
   has_many :groups, through: :enrollments
   has_many :ratings
-  
+
   validates :first_name, :last_name, :email, :password, :grade, presence: true
   validates :grade, inclusion: { in: 1..4, message:  "\"%{value}\" must be between 1 and 4: 1 is First-Year, 2 is Sophomore, etc." }
+
+  # scopes
+  scope :alphabetical, -> { order(:last_name, :first_name) }
+  scope :by_ratin, -> { order("rating desc")}
+
+  private
+
+  def get_user_rating
+    puts "Here's the rating of the user"
+    Rating.find_by(user_id: id)
+  end
 end
