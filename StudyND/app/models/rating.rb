6a28c430ba27e7f95fb969f46cce16fc47c7891c
Gaurav Sirdeshpande <gsirdesh@nd.edu>
2016-12-14 13:50:12 -0500

    Added custom method to Rating model

--- StudyND/app/models/rating.rb
+++ StudyND/app/models/rating.rb
@@ -1,7 +1,14 @@
 class Rating < ActiveRecord::Base
   belongs_to :user
   belongs_to :group
-    
+
   validates :stars, :description, presence: true
   validates :stars, inclusion: {in: 1..5,  message: "\"%{value}\" must be between 1 and 5." }
+
+  private
+
+  def number_of_stars
+    puts "The number of stars: "
+    stars
+  end
 end
