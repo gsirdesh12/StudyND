0fd76c7f757a8bc0c0c4bad1e3af0e04a8b5ab7f
Gaurav Sirdeshpande <gsirdesh@nd.edu>
2016-12-14 13:44:39 -0500

    Added custom validation method to Enrollment model

--- StudyND/app/models/enrollment.rb
+++ StudyND/app/models/enrollment.rb
@@ -1,7 +1,17 @@
 class Enrollment < ActiveRecord::Base
   belongs_to :group
   belongs_to :user
-  
+
   validates :term, :year, presence: true
 
+  validate :validate_year
+
+  private
+
+  def validate_year
+    current_time = Time.new
+    if year < current_time.year
+      errors.add(:year, "can't be in the past of this year")
+    end
+  end
 end
