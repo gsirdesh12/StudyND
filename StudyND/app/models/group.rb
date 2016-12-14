fa84368c58d4a1696a31d4a1eae59d1470fdee10
Esmeralda Cervantes <ecervant@nd.edu>
2016-12-14 02:02:36 -0500

    relationships in group fixed (maybe..)

--- StudyND/app/models/group.rb
+++ StudyND/app/models/group.rb
@@ -1,13 +1,14 @@
 class Group < ActiveRecord::Base
   belongs_to :course
-  has_many :users
+  has_many :users, through: :enrollments
+  has_many :enrollments, dependent: :destroy
   has_many :ratings
-  
+
   validates :name, presence: true
   validates :name, uniqueness: true
 
   def to_s
     name
   end
-  
+
 end
